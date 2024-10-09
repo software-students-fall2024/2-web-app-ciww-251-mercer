235c17023424e7dbdd9a9d941a88607c5a777a96
Luca Ignatescu <lucaignatescu003@gmail.com>
2024-10-09 16:53:33 -0400

    added users database operations

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+from datetime import datetime
 from flask import Flask, render_template
 from pymongo import MongoClient
 from dotenv import load_dotenv
 from os import getenv
 from hashlib import sha256
+from bson import ObjectId
 
 load_dotenv()
 
@@ -14,9 +16,22 @@ if connstr is None:
     raise Exception("Database URI could not be loaded. Check .env file.")
 
 db = MongoClient(connstr)
+collection = db['TODO']['users']
 
+
+'''
+task document
+{
+    task_id: ObjectId
+    title: string
+    description: string
+    due_date: time 
+    completed: boolean
+}
+'''
 class TaskSchema:
     def __init__(self, title, description=None, due_date=None, completed=False):
+        self._id = ObjectId()
         self.title = title
         self.description = description
         self.due_date = due_date
@@ -24,17 +39,25 @@ class TaskSchema:
 
     def to_dict(self):
         return {
+            '_id': str(self._id),
             'title': self.title,
             'description': self.description,
             'due_date': self.due_date,
             'completed': self.completed
         }
-
-
+'''
+user document
+{
+    username: string unique
+    password_hash: string 
+    tasks: Task[]
+}
+'''
 class UserSchema:
-    def __init__(self, username, password):
+    def __init__(self, username, password = None):
         self.username = username
-        self.password_hash = self.hash_password(password)
+        self.password_hash = self.hash_password(password) if password else ""
+        self.tasks = []
 
     def hash_password(self, password):
         return sha256(password.encode()).hexdigest()
@@ -42,8 +65,50 @@ class UserSchema:
     def to_dict(self):
         return {
             'username': self.username,
-            'password_hash': self.password_hash
+            'password_hash': self.password_hash,
+            'tasks': self.tasks
         }
+    
+    @staticmethod
+    def get_user(username):
+        user = collection.find_one({'username': username})
+        if not user:
+            raise Exception("user does not exist")
+
+        record = UserSchema(user['username'])
+        record.password_hash = user['password_hash']
+        record.tasks = user['tasks']
+        return record
+
+    def authenticate(self):
+        user = collection.find_one({'username': self.username})
+        if not user:
+            raise Exception("user does not exist")
+        if self.password_hash != user['password_hash']:
+            raise Exception("password is incorrect")
+
+    def insert_record(self):
+        user = collection.find_one({'username': self.username})
+        if user is not None:
+            raise Exception("username already exists")
+        
+        collection.insert_one(self.to_dict())
+    
+    def add_task(self, task):
+        self.tasks.append(task)
+        collection.update_one(
+            {'username': self.username},
+            {'$push':{'tasks': task.to_dict()}}
+        )
+
+    def get_tasks(self):
+        pass
+
+    def delete_task(self, task_id):
+        pass
+
+    def update_task(self): #idk what args you want here
+        pass
 
 
 @app.route("/")
